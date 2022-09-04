c43421fe6110a92a3c7507ec39b7ea34845df41d
Sourav Paul <souravpl8092@gmail.com>
2022-09-04 20:37:24 +0530

    added

---
+++ day-2-assignment-vi/src/Components/UserProfile.jsx
@@ -0,0 +1,18 @@
+/* function UserProfile(props) {
+  const { src, name, title, skills, logo, desc } = props;
+  const style = {
+    borderRadius: "50%",
+    marginTop: "20px"
+  };
+  return (
+    <div>
+      <img src={src} alt="UserProfile-icon" width="10" style={style} />
+      <h1>{name}</h1>
+      <h2>{title}</h2>
+      <h4>{skills}</h4>
+      <img src={logo} />
+      <p>{desc}</p>
+    </div>
+  );
+}
+export default UserProfile; */
